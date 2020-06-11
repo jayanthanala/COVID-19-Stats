f0eda1a462512832cea79b84352033ed99f6b9ad
Jayanth Anala <63576478+jayanthanala@users.noreply.github.com>
2020-06-11 22:05:26 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const ejs = require("ejs");
+//const ejs = require("ejs");
 const axios = require('axios');
 const app = express();
 
