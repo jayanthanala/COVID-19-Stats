723d63292d11cb202e2d65f3e44cf702bd9ca996
J <JaVa@Js-MacBook-Pro.local>
2020-06-12 21:35:10 +0530

    third commit

--- app.js
+++ app.js
@@ -11,89 +11,111 @@ app.set("view engine", 'ejs');
 app.use(express.static("public"));
 
 ////////////////////////////////////////////// GET Request //////////////////////////////////
-app.get("/",function(req,res){
+app.get("/", function(req, res) {
   axios.get("https://coronavirus-19-api.herokuapp.com/countries")
-  .then(function(response){
-    var country =[];
-    for(var i=0;i<response.data.length;i++){
-      country.push(response.data[i].country);
-    }
-    var tc = numberWithCommas(response.data[0].cases);
-    var td = numberWithCommas(response.data[0].deaths);
-    var ta = numberWithCommas(response.data[0].active);
-    var toc =numberWithCommas(response.data[0].todayCases);
-    var tod =numberWithCommas(response.data[0].todayDeaths);
-    var cr = numberWithCommas(response.data[0].critical);
-    var cpm =numberWithCommas(response.data[0].casesPerOneMillion);
-    var dpm =numberWithCommas(response.data[0].deathsPerOneMillion);
-    res.render("home",{ta:ta,td:td,tc:tc,toc:toc,tod:tod,cr:cr,place:country,cpm:cpm,dpm:dpm});
-  })
-  .catch(function(err){
-    console.log(err);
-  });
+    .then(function(response) {
+      var country = [];
+      for (var i = 0; i < response.data.length; i++) {
+        country.push(response.data[i].country);
+      }
+      var d = response.data[0];
+      res.render("home", {
+        d: d,
+        place: country,
+        fun: numberWithCommas
+      });
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
 });
 
-app.get("/Country",function(req,res){
+app.get("/Country", function(req, res) {
   axios.get("https://coronavirus-19-api.herokuapp.com/countries")
-  .then(function(response){
-    var country =[];
-    for(var i=0;i<response.data.length;i++){
-      country.push(response.data[i].country);
-    }
-    //console.log(country);
-    //New Ideas!! 
-    //REFACTOR THE CODE.
-  res.render("country",{place:country});
-  })
-
+    .then(function(response) {
+      var country = [];
+      for (var i = 0; i < response.data.length; i++) {
+        country.push(response.data[i].country);
+      }
+      res.render("country", {
+        place: country
+      });
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
 });
 
 
-app.get("/World",function(req,res){
+app.get("/World", function(req, res) {
+  var country = [];
+  var totalCases = [];
+  var totalTests = [];
+  var totalRecover = [];
+
   axios.get("https://coronavirus-19-api.herokuapp.com/countries")
-  .then(function(response){
-    var tc = numberWithCommas(response.data[0].cases);
-    var td = numberWithCommas(response.data[0].deaths);
-    var ta = numberWithCommas(response.data[0].active);
-    var tr = numberWithCommas(response.data[0].recovered);
-    var toc =numberWithCommas(response.data[0].todayCases);
-    var tod =numberWithCommas(response.data[0].todayDeaths);
-    var cr = numberWithCommas(response.data[0].critical);
-    var cpm =numberWithCommas(response.data[0].casesPerOneMillion);
-    var dpm =numberWithCommas(response.data[0].deathsPerOneMillion);
-    res.render("world",{ta:ta,tr:tr,td:td,tc:tc,toc:toc,tod:tod,cr:cr});
-  })
+    .then(function(response) {
+
+      var d = response.data[0];
+      var dat = response.data.slice(1,);
+      var color = "dark";
+      //console.log(dat);
+      var data = response.data.slice(1, );
+      //console.log(data);
+      var tc = data.sort(function(a, b) {
+        return b.cases - a.cases
+      }).slice(0, 5);
+      var td = data.sort(function(a, b) {
+        return b.deaths - a.deaths
+      }).slice(0, 5);
+      var tr = data.sort(function(a, b) {
+        return b.recovered - a.recovered
+      }).slice(0, 5);
+      var ta = data.sort(function(a, b) {
+        return b.active - a.active
+      }).slice(0, 5);
+      res.render("world", {
+        d: d,
+        fun: numberWithCommas,
+        tc: tc,
+        td: td,
+        tr: tr,
+        ta: ta,
+        data:dat,
+        tbu: "To-Be-Updated",
+        bg: color
+      });
+    })
 });
 
 ////////////////////////////////////////////// POST Request //////////////////////////////////
 
-app.post("/place",function(req,res){
+app.post("/place", function(req, res) {
   var place = req.body.place;
   // console.log(place);
-  axios.get("https://coronavirus-19-api.herokuapp.com/countries/"+place)
-  .then(function(response){
-    var country = response.data.country;
-    var tc = numberWithCommas(response.data.cases);
-    var td = numberWithCommas(response.data.deaths);
-    var ta = numberWithCommas(response.data.active);
-    var tr = numberWithCommas(response.data.recovered);
-    var toc =numberWithCommas(response.data.todayCases);
-    var tod =numberWithCommas(response.data.todayDeaths);
-    res.render("place",{ta:ta,td:td,tc:tc,toc:toc,tod:tod,tr:tr,place:country});
-  })
-  .catch(function(err){
-    console.log(err);
-  });
+  axios.get("https://coronavirus-19-api.herokuapp.com/countries/" + place)
+    .then(function(response) {
+      var country = response.data.country;
+      var d = response.data;
+      res.render("place", {
+        d: d,
+        fun: numberWithCommas,
+        place: country
+      });
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
 });
 
 
 
+////////////////////////////////////////////// PORTS /////////////////////////////////////////
 
-
-app.listen(3000,function(){
+app.listen(3000, function() {
   console.log("Server is up at 3000");
 });
 
 function numberWithCommas(x) {
-    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
